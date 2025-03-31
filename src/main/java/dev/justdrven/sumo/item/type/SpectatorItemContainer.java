41738be817c702a9621b708822132836bfbb8766
JustDrven <petr.nemec11111@gmail.com>
2025-03-31 18:07:38 +0200

    feat(commands): created spectator container

---
+++ src/main/java/dev/justdrven/sumo/item/type/SpectatorItemContainer.java
@@ -0,0 +1,12 @@
+package dev.justdrven.sumo.item.type;
+
+import dev.justdrven.sumo.item.ItemContainer;
+import dev.justdrven.sumo.player.GamePlayer;
+import org.bukkit.inventory.Inventory;
+
+public class SpectatorItemContainer implements ItemContainer {
+    @Override
+    public void giveItems(GamePlayer gamePlayer, Inventory inv) {
+
+    }
+}
